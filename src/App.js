825cf8b52b8e7a35f6ee35b4d6ea7862640abc8b
root <root@ip-172-31-41-123.us-east-2.compute.internal>
2019-11-05 21:30:35 +0000

    docker container

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Added text <i>Just now</i> <b>Latest</b> added more text
         </p>
         <a
           className="App-link"
